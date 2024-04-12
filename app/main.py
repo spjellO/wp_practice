dae08bd33f6986caa615a1e392701bebce7f11b3
채주원 <wndnjs4698@naver.com>
2024-04-12 16:35:50 +0900

    quest

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+global item
 
 @app.get("/")
 def read_root():
@@ -13,3 +14,15 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/name/{name}")
+def insert(name: str):
+    item = name
+    return {"message": "your name has been saved"}
+
+@app.get("/namelist/{name}")
+def select(name: str):
+    if item == name:
+        return {"message" : "name that you've saved is" + name}
+    else :
+        return {"message" : "name cannot be found"}
