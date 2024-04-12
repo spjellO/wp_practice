dd1a0fb33d2588d7f636b1818760f200683f69a7
채주원 <wndnjs4698@naver.com>
2024-04-12 17:08:15 +0900

    test

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# name = ""
-
 class item():
     name = ""
 
@@ -13,26 +11,26 @@ class item():
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/name/{name}")
+@app.get("/name_wr/{name}")
 def select_name(name: item):
     if name is item.name:
         return { "name": name }
     else:
         return { "name": "name cannot be found"}
 
-@app.post("/name/{name}")
+@app.post("/name_sel/{name}")
 def write_name(name: item):
     item.name = name
     return { "name": "your name saved"}
 
-@app.put("/name/{name}")
+@app.put("/name_up/{name}")
 def update_name(name: item):
     if name is item.name:
         item.name = name
     else:
         return { "name": "name cannot be found"}
 
-@app.delete("/name/{name}")
+@app.delete("/name_del/{name}")
 def delete_name(name: item):
     if name is item.name:
         item.name = ""
