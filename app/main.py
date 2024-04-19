1a88ed92e342b9212bfeaa47164d51b008afb6e5
채주원 <wndnjs4698@naver.com>
2024-04-19 15:50:13 +0900

    test

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 from typing import Union
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
 item = None
 
 @app.get("/")
+def root():
+    key = 'duqNoXzM431om35edqH7Qhcrd9M3uYYFm00ZV94K'
+    URL = f"https://bigdata.kepco.co.kr/openapi/v1/powerUsage/contractType.do?year=2020&month=11&metroCd=11&cityCd=110&cntrCd=100&apiKey={key}&returnType=json"
+
+    contents = requests.get(URL).text
+
+    return { "message": contents }
+
+@app.get("/name")
 def select_name(name: str):
     global item
     if name == item:
@@ -13,13 +23,13 @@ def select_name(name: str):
     else:
         return { "name": f" {name} name cannot be found"}
 
-@app.post("/")
+@app.post("/name")
 def write_name(new_name: str):
     global item
     item = new_name
     return { "name": f"your name {new_name} saved"}
 
-@app.put("/")
+@app.put("/name")
 def update_name(updated_name: str):
     global item
     if item:
@@ -28,7 +38,7 @@ def update_name(updated_name: str):
     else:
         return { "name": "name to update do not exist"}
 
-@app.delete("/")
+@app.delete("/name")
 def delete_name(name: str):
     global item
     if item:
