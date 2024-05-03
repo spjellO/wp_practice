5a6eaebcd0aef19b067866a48f3767f514048c07
채주원 <wndnjs4698@naver.com>
2024-05-03 15:45:32 +0900

    db

--- app/main.py
+++ app/main.py
@@ -1,48 +1,73 @@
-from typing import Union
-from fastapi import FastAPI
-import requests
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from pydantic import BaseModel
+
+SQLALCHEMY_DATABLSE_URL = "sqlite:///../todo.db"
+engine = create_engine(SQLALCHEMY_DATABLSE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 app = FastAPI()
 
-item = None
+class Todo(Base):
+    __tablename__ = "todos"
+    __allow_unmapped__ = True
 
-@app.get("/")
-def root(key: str):
-    URL = f"https://bigdata.kepco.co.kr/openapi/v1/powerUsage/contractType.do?year=2023&month=11&metroCd=11&cityCd=110&apiKey={key}&returnType=json"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
 
-    contents = requests.get(URL).text
+Base.metadata.create_all(bind=engine)
 
-    return { "message": contents }
+class TodoCreate(BaseModel):
+    title: str
+    description: str
 
-@app.get("/name")
-def select_name(name: str):
-    global item
-    if name == item:
-        return { "name": item }
-    else:
-        return { "name": f" {name} name cannot be found"}
+class TodoUpdate(BaseModel):
+    title: str
+    description: str
 
-@app.post("/name")
-def write_name(new_name: str):
-    global item
-    item = new_name
-    return { "name": f"your name {new_name} saved"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.put("/name")
-def update_name(updated_name: str):
-    global item
-    if item:
-        item = updated_name
-        return { "name": f"name updated into {updated_name} successfully"}
-    else:
-        return { "name": "name to update do not exist"}
+@app.post("/todos/")
+def create_todo(todo: TodoCreate, db: Session = Depends(get_db)):
+    db_todo = Todo(title=todo.title, description=todo.description)
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
 
-@app.delete("/name")
-def delete_name(name: str):
-    global item
-    if item:
-        item = None
-        return { "name": "your name deleted successfully"}
-    else:
-        return { "name": "name cannot be found"}
-    
+@app.get("/todos/{todo_id}")
+def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return db_todo
+
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id: int, todo: TodoUpdate, db: Session = Depends(get_db)):
+    db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if db_todo is None: 
+        raise HTTPException(status_code=404, detail="Todo not found")
+    db_todo.title = todo.title
+    db_todo.description = todo.description
+    db.commit()
+    return db_todo
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+    db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    db.delete(db_todo)
+    db.commit()
+    return {"ok": True}
